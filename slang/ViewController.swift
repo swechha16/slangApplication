d8c7b8742ff07a59314899073b38f69da0b60f1e
Swechha <swechha@sanjays-macbook-air.local>
2018-08-02 10:32:13 -0500

    Initial Commit

---
+++ slang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  slang
+//
+//  Created by Swechha on 8/2/18.
+//  Copyright © 2018 Swechha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
